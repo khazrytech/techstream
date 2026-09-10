f6d4aeb3602da06a397e1e32a976c1a74de66464
khazrytech <hackertrick1997@gmail.com>
2026-09-11 00:07:47 +0300

    Ignore typescript errors during build for vercel deployment

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Tunaruhusu Vercel itumie default building yake bila kulazimisha standalone traces
-};
-
-export default nextConfig;
